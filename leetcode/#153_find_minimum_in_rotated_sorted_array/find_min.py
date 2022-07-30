372e6870a600a01aa3ff7a2c4bb4ba649e82ae68
Kyle Shepard <kshepard379@gmail.com>
2022-07-30 13:42:03 -0700

    log n solution

---
+++ leetcode/#153_find_minimum_in_rotated_sorted_array/find_min.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        
+        l, r = 0, len(nums) - 1
+        
+        # if length is one or array is not rotated
+        if nums[l] <= nums[r]:
+            print(nums, nums[l])
+            return nums[l]
+        
+        # if l and r are next to each other, r will be the index of the min
+        while l < r-1:
+            
+            pivot = l + (r - l) // 2
+
+            if nums[pivot] > nums[l]:
+                l = pivot
+            else:
+                r = pivot
+        
+        return nums[r]
---
+++ leetcode/#153_find_minimum_in_rotated_sorted_array/readme.md
@@ -0,0 +1,9 @@
+Problem [#153](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/)
+
+Difficulty: Medium
+
+Language used: Python3
+
+Efficiency: O(log n)
+
+See [Code Submission](https://leetcode.com/submissions/detail/760926436/)
